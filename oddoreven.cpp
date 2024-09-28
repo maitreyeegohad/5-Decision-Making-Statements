b38c9200261d70cb9d866cd89b41720f6814e07b
Maitreyee Gohad <maitreyee.gohad@gmail.com>
2024-09-28 11:40:16 +0530

    Create oddoreven.cpp

---
+++ oddoreven.cpp
@@ -0,0 +1,29 @@
+//Maitreyee Gohad
+//24070123509
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int a;
+    cout<<"Enter the number: ";
+    cin>>a;
+
+    if(a%2 == 0)
+    {
+        cout<<"It is an even number.";
+    }
+    else
+    {
+        cout<<"It is an odd number.";
+    }
+}
+
+/*OUTPUT: 
+Enter the number: 
+9
+It is an odd number.
+
+Enter the number: 
+7462
+It is an even number. */
